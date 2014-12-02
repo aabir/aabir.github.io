0ee6c7cd35222d61bef1e22f9414b7abf58cdc99
aabir <shblnmn@gmail.com>
2014-12-02 12:11:30 +0600

    Updates

---
+++ index.php
@@ -0,0 +1,461 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1"/>
+<title> Welcome to Shible Noman Aabir's Protfolio </title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="description" content="4 years experience as full-stack web and software developer. Self taught web application developer. ">
+<meta name="keywords" content="full-stack developer, web developer, software developer, e-commerce developer, freelancer , outsourcing developer, HTML5, CSS3, css animation, wordpress custom theme developer, data intensive application, PHP development, mobile web development, html5 mobile websites, responsive web design, CodeIgniter developer, CakePHP developer, Laravel developer" />
+<meta name="author" content="Shible Noman Aabir, shblnmn@gmail.com">
+<meta name="robots" content="index, follow" />
+<meta name="programmer" content="Shible Noman Aabir" />
+<meta name="format-detection" content="telephone=no">
+<!-- Facebook -->
+<meta property="og:title" content="Shible Noman Aabir Portfolio" />
+<meta property="og:site_name" content="Shible Noman Aabir Portfolio" />
+<meta property="og:description" content="4 years experience as full-stack web and software developer. Self taught web application developer. " />
+<meta property="og:url" content="http://shiblenoman.com" />
+<meta property="og:image" content="http://shiblenoman.com/images/facebook_share.png" />
+<meta property="og:type" content="website" />
+<!-- Google Plus -->
+<meta itemprop="name" content="Shible Noman Aabir Portfolio" />
+<meta itemprop="description" content="" />
+<meta itemprop="image" content="http://shiblenoman.com/images/facebook_share.png" />
+<!-- Twitter -->
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@to_be_aabir" />
+<meta name="twitter:creator" content="@to_be_aabir" />
+<meta name="twitter:title" content="Shible Noman Aabir Portfolio" />
+<meta name="twitter:description" content="4 years experience as full-stack web and software developer. Self taught web application developer. " />
+<meta name="twitter:image" content="http://shiblenoman.com/images/facebook_share.png" />
+<meta name="twitter:url" content="http://shiblenoman.com" />
+
+<!-- styles -->
+<link href="css/normalize.css" rel="stylesheet">
+<link href="css/app.css" rel="stylesheet">
+<link href="css/magnific-popup.css" rel="stylesheet">
+
+<!-- styles End -->
+
+<!--[if (gte IE 6)&(lte IE 8)]>
+  <script type="text/javascript" src="js/selectivizr-min.js"></script>
+<![endif]-->
+
+<!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+<![endif]-->
+
+</head>
+
+<div class="spinner">
+  <div class="cube1"></div>
+  <div class="cube2"></div>
+</div>
+
+<body onload="hello();">
+
+<div id="body" style="display: none; height: 100%;">
+
+<header id="header">
+
+<div class="container">
+	<nav id="nav" style="position: relative;">
+		<a id="nav-toggle" href="javascript:void(0)"><span></span></a>
+		
+		<ul id="show">
+			<li><a href="#intro" class="clicked">Top </a>
+			<li><a href="#about" id="" class="clicked">About </a>
+			<li><a href="#portfolio" class="clicked">Projects </a>
+			<li><a href="#contact" class="clicked">Contact </a>
+		</ul>
+		
+	</nav>
+</div>
+
+</header>
+
+<section id="intro" class="fullscreen background" data-type="background" data-speed="8">
+	<div class="overlay" style=""></div>
+    
+    <div class="topFixed">
+    
+		<div class="container wow fade" id="first">
+		
+        <div id="hello" class="hello"></div>
+		
+		<p>I'm <strong> Shible Noman Aabir</strong>
+		I love making things for human mind in machine language. </p>
+		
+        <div class="social-box">
+        	<div class="social">
+                <ul>
+                    <li> <a href="mailto:shblnmn@gmail.com" target="_blank"> <span class="icon-mail"></span> </a> </li>
+                    <li> <a href="skype:nomanaabir" target="_blank"> <span class="icon-skype"></span> </a> </li>
+                    <li> <a href="https://github.com/aabir" target="_blank"> <span class="icon-github-circled-alt2"> </span> </a></li>
+                </ul>
+            </div>
+        </div>
+		
+	</div>
+    
+    </div> <!-- to text fixed -->
+    
+    <div class="down-holder">
+        <a href="#about" class="button scroll">
+            <span class="icon-down-open"></span>
+        </a>
+    </div>
+    
+</section>
+
+
+
+<section id="about">
+
+<div class="container">
+	<h2> About Me</h2>
+	
+	
+    <div class="v-center">
+    
+        <div class="skill-left wow inLeft">
+            <p> I have been working almost 4 year in web technology. My primary server side programming language is PHP with Codeigniter framework and I manage backend using MySQL. I have reasonable knowledge of CakePHP and Laravel PHP framework. Project requirement analysis and database design is part of my work. The client side technologies that I use are CSS with SASS preprocessor, JavasScript with Jquery library and Ajax. I love to discover and use HTML5 and CSS3 power to latest development trend. I have an expert hand in custom build WordPress CMS themes. with experience in other CMS like OpenCart, Magento and Joomla. I use GIT and GitHub for collaboration. <br /><br /> I also love to read about latest web technologies, technic and tutorials through my RSS reader.
+      </p>
+        </div>
+	
+		<div class="skill-right">
+		<div class="big-circle wow zoomIn">
+		   
+			<div class="small-circle wow one" id="html"><i class="icon-html5-1"></i></div>
+			<div class="small-circle wow two" id="css"><i class="icon-css3"></i></div>
+			<div class="small-circle wow three" id="js"><i class="icon-javascript"></i></div>
+			<div class="small-circle wow four" id="php"><i class="icon-php-alt"></i></div>
+			<div class="small-circle wow five" id="mysql"><i class="icon-mysql-alt"></i></div>
+			<div class="small-circle wow six" id="wp"><i class="icon-wordpress"></i></div>
+		   
+            <div class="center-circle"> <div class="skill-text"> </div></div>
+           
+           
+		</div>
+	</div>
+    
+    </div>
+
+</div>
+
+</section>
+
+<section id="portfolio">
+
+<div class="container">
+	<h2> Projects </h2>
+</div>
+
+<div class="grid">
+
+	<figure class="effect wow portfolio-In-1">
+		<img src="images/projects/botter.png" alt="Botter"/>
+		<figcaption>
+			<h3>Botter Wine</h3>
+			<p><span class="icon-popup"><a href="#portfolio1">&nbsp;</a></span></p>
+		</figcaption>			
+	</figure>
+	
+	<figure class="effect wow portfolio-In-2">
+		<img src="images/projects/ilcaneistruito.png" alt="Ilcaneistruito" />
+		<figcaption>
+			<h3>Ilcaneistruito</h3>
+			<p><span class="icon-popup"><a href="#portfolio2">&nbsp;</a></span></p>
+		</figcaption>			
+	</figure>
+	
+	<figure class="effect wow portfolio-In-3">
+		<img src="images/projects/freehousevaluer.png" alt="FreeHouseValuer"/>
+		<figcaption>
+			<h3>Freehousevaluer</h3>
+			<p><span class="icon-popup"><a href="#portfolio3">&nbsp;</a></span></p>
+		</figcaption>			
+	</figure>
+	
+	<figure class="effect wow portfolio-In-4">
+		<img src="images/projects/bartaprobaho.png" alt="Barta probaho"/>
+		<figcaption>
+			<h3>Barta Probaho</h3>
+			<p> <span class="icon-popup"><a href="#portfolio4">&nbsp;</a></span> </p>
+		</figcaption>			
+	</figure>
+	
+	<figure class="effect wow portfolio-In-1">
+		<img src="images/projects/RTCL.png" alt="RTCL Properties and Developers"/>
+		<figcaption>
+			<h3>RTCL</h3>
+			<p> <span class="icon-popup"><a href="#portfolio5">&nbsp;</a></span> </p>
+		</figcaption>			
+	</figure>
+	
+	<figure class="effect wow portfolio-In-2">
+		<img src="images/projects/innatseaside.png" alt="Inn at SeaSide"/>
+		<figcaption>
+			<h3>Inn at Sea Side</h3>
+			<p> <span class="icon-popup"><a href="#portfolio6">&nbsp;</a></span> </p>
+		</figcaption>			
+	</figure>
+	<figure class="effect wow portfolio-In-3">
+		<img src="images/projects/bup.png" alt="Bangladesh University Of Professionals (BUP)" />
+		<figcaption>
+			<h3>BUP</span></h3>
+			<p> <span class="icon-popup"><a href="#portfolio7">&nbsp;</a></span> </p>
+		</figcaption>			
+	</figure>
+	
+	<figure class="effect wow portfolio-In-4">
+		<img src="images/projects/rms.png" alt="Recruitment Management System (RMS) "/>
+		<figcaption>
+			<h3>RMS</span></h3>
+			<p><span class="icon-popup"><a href="#portfolio8">&nbsp;</a></span></p>
+		</figcaption>			
+	</figure>
+  
+</div>
+
+<!-- popup content --> 
+
+<div class="test-popup white-popup mfp-hide" id="portfolio1">  
+
+  <div class="pop-image">
+	<img src="images/projects/botter.png" alt="Botter"/>
+  </div>
+  
+  <div class="pop-desc">
+	<h3> What I Performed </h3>
+	<ul>
+		<li> Converted the design into HTML and CSS </li>
+		<li> Made the design device independent (RWD) </li>
+		<li> Coded for WordPress CMS </li>
+		<li> Used Ajax to fetch content </li>
+		<li> WP custom post type, post meta box and theme options used </li>
+	</ul>
+  </div>
+  
+</div>
+
+<div class="test-popup white-popup mfp-hide" id="portfolio2">  
+
+  <div class="pop-image">
+	<img src="images/projects/ilcaneistruito.png" alt="Ilcaneistruito" />
+  </div>
+  
+  <div class="pop-desc">
+	<h3> What I Performed </h3>
+	<ul>
+		<li> Converted the design into HTML and CSS </li>
+		<li> Made the design device independent (RWD) </li>
+		<li> Coded for WordPress CMS </li>
+		<li> WP custom post type, post meta box and theme options used </li>
+		<li> WP WooCommerce plugin integration and customization </li>
+	</ul>
+  </div>
+  
+</div>
+
+<div class="test-popup white-popup mfp-hide" id="portfolio3">  
+
+  <div class="pop-image">
+	<img src="images/projects/freehousevaluer.png" alt="Freehouse Valuer "/>
+  </div>
+  
+  <div class="pop-desc">
+	<h3> What I Performed </h3>
+	<ul>
+		<li> Converted the design into HTML and CSS </li>
+        <li> Made the design device independent (RWD) </li>
+		<li> Coded for WordPress CMS </li>
+		<li> WP custom post type, shortcode, post meta box and theme options used </li>
+		<li> Ajax submitted forms and sent form data through email </li>
+		<li> MailChimp integration </li>
+	</ul>
+  </div>
+  
+</div>
+
+<div class="test-popup white-popup mfp-hide" id="portfolio4">  
+
+  <div class="pop-image">
+	<img src="images/projects/bartaprobaho.png" alt="Barta Probaho"/>
+  </div>
+  
+  <div class="pop-desc">
+	<h3> What I Performed </h3>
+	<ul>
+    	<li> Planning, designing and coding </li>
+    	<li> Requirement analysis, database design </li>
+		<li> Front end and back end coding in CodeIgniter(CI) </li>
+		<li> BootStrap CSS framework used for RWD </li>
+		<li> Admin panel for content upload </li>
+	</ul>
+  </div>
+  
+</div>
+
+<div class="test-popup white-popup mfp-hide" id="portfolio5">  
+
+  <div class="pop-image">
+	<img src="images/projects/RTCL.png" alt="RTCL Properties and Developers Ltd."/>
+  </div>
+  
+  <div class="pop-desc">
+	<h3> What I Performed </h3>
+	<ul>
+    	<li> Design is all device compatible(RWD) </li>
+        <li> Requirement analysis, database design </li>
+		<li> Front end and back end coding in CodeIgniter(CI) </li>
+        <li> Admin panel for content upload </li>
+	</ul>
+  </div>
+  
+</div>
+
+<div class="test-popup white-popup mfp-hide" id="portfolio6">  
+
+  <div class="pop-image">
+	<img src="images/projects/innatseaside.png" alt="Inn at SeaSide" />
+  </div>
+  
+  <div class="pop-desc">
+	<h3> What I Performed </h3>
+	<ul>
+		<li> Converted the design into HTML and CSS </li>
+        <li> Front end and back end coding in CodeIgniter(CI) </li>
+		<li> Admin panel for content upload </li>
+		<li> Booking API integreation </li>
+	</ul>
+  </div>
+  
+</div>
+
+<div class="test-popup white-popup mfp-hide" id="portfolio7">  
+
+  <div class="pop-image">
+	<img src="images/projects/bup.png" alt="Bangladesh University of Professionals (BUP)"/>
+  </div>
+  
+  <div class="pop-desc">
+	<h3> What I Performed </h3>
+	<ul>
+		<li> University management system for Bangladesh University of Professinals(BUP) </li>
+		<li> Worked as core programmer in a team </li>
+		<li> Developed two modules individually </li>
+        <li> Built on top of CodeIgniter(CI)</li>
+		<li> Wrote database trigger, query and CI code merge with the team members </li>
+	</ul>
+  </div>
+  
+</div>
+
+<div class="test-popup white-popup mfp-hide" id="portfolio8">  
+
+  <div class="pop-image">
+	<img src="images/projects/rms.png" alt="Recruitment Management System (RMS)"/>
+  </div>
+  
+  <div class="pop-desc">
+	<h3> What I Performed </h3>
+	<ul>
+		<li> Recruitment management system (RMS) </li>
+		<li> Built as product for medium to large companies for bulk recruitment </li>
+		<li> Built on top of CodeIgniter(CI)</li>
+		<li> I alone build the project </li>
+		<li> Wrote database trigger, query and CI code </li>
+	</ul>
+  </div>
+  
+</div>
+
+<!-- popup content end --> 
+
+
+</section>
+
+<section id="contact">
+	<div class="contact-bg">
+	
+		<div class="container">
+		<h2>Contact</h2>
+        	<div id="form-success"></div>
+			<div class="contact-wrapper">
+				<div class="form-wrapper">
+					<div class="contact-form">
+					<form method="post" id="contact-form" class="wow contact-in">
+								<div>
+									
+									<span><input name="userName" type="text" class="textbox" placeholder="Name" parsley-required="true" parsley-error-message=""></span>
+								</div>
+								<div>
+									
+									<span><input name="userEmail" type="text" class="textbox" placeholder="Email" parsley-required="true" parsley-error-message="" parsley-type="email"></span>
+								</div>
+								<div>
+									
+									<span> <textarea id="userMessage" name="userMessage" placeholder="Massage" parsley-required="true" parsley-error-message=""></textarea> </span>
+								</div>
+							   
+                               <input type="hidden" id="contact_form_recipient" name="contact_form_recipient" value="shblnmn@gmail.com" class="" />
+                               
+								<button class="sub"> Send </button>
+							 
+						</form>			
+					</div>
+				</div>
+			</div>
+			
+		
+		</div>
+	</div>
+</section>
+
+<footer id="footer">
+
+	<div class="social">
+		<ul>
+			<li> <a href="mailto:shblnmn@gmail.com" target="_blank"> <span class="icon-mail"></span> </a> </li>
+			<li> <a href="skype:nomanaabir" target="_blank"> <span class="icon-skype"></span> </a> </li>
+			<li> <a href="https://github.com/aabir" target="_blank"> <span class="icon-github-circled-alt2"> </span> </a></li>
+		</ul>
+		
+	</div>
+    
+	<div class="footer-credit">
+    	<p> Hand Crafted With Love </p>
+    </div>
+    
+</footer>
+
+</div>  <!-- Body ID End --> 
+
+<!-- include js -->
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<!-- fall back to local if offline -->
+<script>window.jQuery || document.write('<script src="js/jquery-1.11.0.min.js"><\/script>')</script>
+<script src="js/raphael.js"></script>
+<script src="js/jquery.lazylinepainter-1.4.1.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/parsley.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+<!-- include js -->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57185992-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+</html>
--- js/custom.js
+++ js/custom.js
@@ -12,6 +12,8 @@ $(function(){
 	$(window).load(function(){
 		$('.spinner').fadeOut(500);
 		$('#body').css('display', 'block');
+		/* Animation WOW */
+		new WOW().init();
 	});
 });
 
@@ -31,10 +33,7 @@ $(function(){
 	});
 });
 
-/* Animation Init */
-$(function(){
-	new WOW().init();
-});
+
 
 /* Menu Scroll */
 $(document).ready(function(){
@@ -107,7 +106,7 @@ $(function(){
 		} else if(id == "mysql"){
 			textDiv.text(" MySQL for backend memory ");	
 		} else if(id == "wp"){
-			textDiv.text(" WordPress custom theme development, custom post type, meta box and theme options with an insightful and authentic way. ");		
+			textDiv.text(" WordPress custom theme development, custom post type, meta box, shortcode and theme options with an insightful and authentic way. ");		
 		}
 		
 	});
@@ -170,21 +169,115 @@ jQuery(function($) {
         var st = $(this).scrollTop();
         divs.css({ 
             //'margin-top' : -(st/3)+"px", 
-            'opacity' : 1 - st/300
+            'opacity' : 1 - st/400
         }); 
     });
 });
 
+var animate = function(){
+	$('#hello').lazylinepainter({
+		"svgData": pathObj,
+		"strokeWidth":2,
+		"strokeColor": "#f5f5f5",
+		"delay": 1500
+	}).lazylinepainter('paint'); 
+};
 
-/* Page Visibility Effect */ 
- $(function() {
-	$(document).on({
-		'show.visibility': function() {
-			$("#first").addClass("fadeOut");
-		}/*,
-		'hide.visibility': function() {
-			$("#first").removeClass("fadeOut");
-		}*/
-	});
+var pathObj = {
+    "hello": {
+        "strokepath": [
+            {
+                "path": "m 9.2460938,53 0,-44.3808594 5.8730472,0 0,18.2246094 23.068359,0 0,-18.2246094 5.873047,0 0,44.3808594 -5.873047,0 0,-20.918945 -23.068359,0 0,20.918945 z",
+                "duration": 600
+            },
+            {
+                "path": "m 75.181641,42.646484 5.630859,0.696289 c -0.888052,3.28972 -2.532907,5.842778 -4.93457,7.65918 -2.401717,1.816407 -5.469423,2.724609 -9.203125,2.724609 -4.702487,0 -8.431162,-1.448078 -11.186035,-4.344238 -2.754887,-2.896152 -4.132327,-6.957834 -4.132325,-12.185058 -2e-6,-5.408833 1.392575,-9.606746 4.177735,-12.59375 2.785147,-2.986948 6.397773,-4.480436 10.83789,-4.480469 4.298807,3.3e-5 7.810522,1.463247 10.535157,4.389648 2.724578,2.926458 4.086882,7.043642 4.086914,12.351563 -3.2e-5,0.322932 -0.01012,0.807307 -0.03027,1.453125 l -23.976562,0 c 0.201815,3.531912 1.200837,6.236336 2.99707,8.113281 1.796211,1.876958 4.036443,2.815434 6.720703,2.81543 1.998027,4e-6 3.703429,-0.524736 5.116211,-1.574219 1.412736,-1.049473 2.532852,-2.724601 3.360352,-5.025391 z m -17.891602,-8.80957 17.952148,0 c -0.242213,-2.704405 -0.92841,-4.732723 -2.058593,-6.084961 -1.7357,-2.098931 -3.986023,-3.148409 -6.750977,-3.148437 -2.502619,2.8e-5 -4.606621,0.837592 -6.312012,2.512695 -1.705413,1.675154 -2.648934,3.915386 -2.830566,6.720703 z",
+                "duration": 1800
+            },
+            {
+                "path": "m 87.563477,53 0,-44.3808594 5.449218,0 0,44.3808594 z",
+                "duration": 600
+            },
+            {
+                "path": "m 101.36816,53 0,-44.3808594 5.44922,0 0,44.3808594 z",
+                "duration": 600
+            },
+            {
+                "path": "m 113.26563,36.924805 c -1e-5,-5.953754 1.65494,-10.363581 4.96484,-13.229493 2.76496,-2.381478 6.1354,-3.572232 10.11133,-3.572265 4.4199,3.3e-5 8.03252,1.448111 10.83789,4.344238 2.80531,2.896185 4.20797,6.89732 4.20801,12.003418 -4e-5,4.137382 -0.62064,7.391774 -1.86182,9.763184 -1.24124,2.371423 -3.04755,4.213056 -5.41895,5.524902 -2.37144,1.311849 -4.95981,1.967773 -7.76513,1.967773 -4.50067,0 -8.13852,-1.443033 -10.91358,-4.329101 -2.77507,-2.886061 -4.1626,-7.043609 -4.16259,-12.472656 z m 5.60058,0 c -10e-6,4.117199 0.8981,7.200041 2.69434,9.248535 1.79621,2.048507 4.05662,3.072758 6.78125,3.072754 2.7044,4e-6 4.95473,-1.029292 6.75097,-3.087891 1.7962,-2.058585 2.69431,-5.196928 2.69434,-9.415039 -3e-5,-3.975891 -0.90318,-6.988095 -2.70947,-9.036621 -1.80634,-2.048475 -4.05162,-3.072726 -6.73584,-3.072754 -2.72463,2.8e-5 -4.98504,1.019233 -6.78125,3.057617 -1.79624,2.038435 -2.69435,5.116231 -2.69434,9.233399 z",
+                "duration": 1800
+            }
+        ],
+        "dimensions": {
+            "width": 150,
+            "height": 65
+        }
+    }
+}; 
+
+
+// Get Browser-Specifc Prefix
+function getBrowserPrefix() {
+   
+  // Check for the unprefixed property.
+  if ('hidden' in document) {
+    return null;
+  }
+ 
+  // All the possible prefixes.
+  var browserPrefixes = ['moz', 'ms', 'o', 'webkit'];
+ 
+  for (var i = 0; i < browserPrefixes.length; i++) {
+    var prefix = browserPrefixes[i] + 'Hidden';
+    if (prefix in document) {
+      return browserPrefixes[i];
+    }
+  }
+ 
+  // The API is not supported in browser.
+  return null;
+}
+ 
+// Get Browser Specific Hidden Property
+function hiddenProperty(prefix) {
+  if (prefix) {
+    return prefix + 'Hidden';
+  } else {
+    return 'hidden';
+  }
+}
+ 
+// Get Browser Specific Visibility State
+function visibilityState(prefix) {
+  if (prefix) {
+    return prefix + 'VisibilityState';
+  } else {
+    return 'visibilityState';
+  }
+}
+ 
+// Get Browser Specific Event
+function visibilityEvent(prefix) {
+  if (prefix) {
+    return prefix + 'visibilitychange';
+  } else {
+    return 'visibilitychange';
+  }
+}
+
+// Get Browser Prefix
+var prefix = getBrowserPrefix();
+var hidden = hiddenProperty(prefix);
+var visibilityState = visibilityState(prefix);
+var visibilityEvent = visibilityEvent(prefix);
+ 
+document.addEventListener(visibilityEvent, function(event) {
+  if (!document[hidden]) {
+		$(function(){animate();})
+  } 
 });
-	
+
+function hello(){
+	if (!document[hidden]) {
+		$(function(){animate();})
+	} 	
+}
